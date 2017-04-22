bd295bc4504b81cb3eaf039c0bd5cceb1bd4e6bb
Qing Dai <qingdai54@gmail.com>
2017-04-22 01:57:02 -0700

    first commit

---
+++ Assignment_1/Assignment_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment_1
+//
+//  Created by Qing Dai on 4/22/17.
+//  Copyright © 2017 Qing Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
